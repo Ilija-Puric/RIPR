a6305bd40eb8f297c5eb1a77eb503b383bc4e20b
Shinigaaami <dishonerd121@gmail.com>
2021-11-30 23:36:13 +0100

    nzm

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-const hamburger = document.querySelector(".hamburgerIkona");
-const listaNav = document.querySelectorAll(".preHamburger");
-var kliknuto = false;
+// const hamburger = document.querySelector(".hamburgerIkona");
+// const listaNav = document.querySelectorAll(".preHamburger");
+// var kliknuto = false;
 
-console.log("Begin " + kliknuto);
-if (kliknuto == false)
-    hamburger.addEventListener("click", e => {
-        if (!kliknuto) {
-            console.log("unutra 1 pre " + kliknuto);
+// console.log("Begin " + kliknuto);
+// if (kliknuto == false)
+//     hamburger.addEventListener("click", e => {
+//         if (!kliknuto) {
+//             console.log("unutra 1 pre " + kliknuto);
 
-            const nav = document.getElementById("navBar");
-            nav.style.flexDirection = "column ";
+//             const nav = document.getElementById("navBar");
+//             nav.style.flexDirection = "column ";
 
 
-            const ulList = document.querySelector(".listeFlex");
-            ulList.style.flexDirection = "column";
-            ulList.style.gap = "20px";
+//             const ulList = document.querySelector(".listeFlex");
+//             ulList.style.flexDirection = "column";
+//             ulList.style.gap = "20px";
 
-            const listItems = document.getElementsByClassName("preHamburger");
-            for (var i = 0; i < listItems.length; i++) {
-                listItems[i].style.display = "block";
-            }
+//             const listItems = document.getElementsByClassName("preHamburger");
+//             for (var i = 0; i < listItems.length; i++) {
+//                 listItems[i].style.display = "block";
+//             }
 
-            hamburger.style.paddingLeft = "50px";
+//             hamburger.style.paddingLeft = "50px";
 
-            kliknuto = true;
-            console.log("unutra 1 " + kliknuto);
-            if (kliknuto == true)
-                hamburger.addEventListener("click", e => {
-                    if (kliknuto) {
+//             kliknuto = true;
+//             console.log("unutra 1 " + kliknuto);
+//             if (kliknuto == true)
+//                 hamburger.addEventListener("click", e => {
+//                     if (kliknuto) {
 
-                        for (var i = 0; i < listItems.length; i++) {
-                            listItems[i].style.display = "none";
-                        }
+//                         for (var i = 0; i < listItems.length; i++) {
+//                             listItems[i].style.display = "none";
+//                         }
                        
-                        nav.style.flexDirection = "row";
+//                         nav.style.flexDirection = "row";
 
-                        console.log("unutra 2 "+ kliknuto);
-                        kliknuto = false;
-                        console.log("unutra 2 end "+ kliknuto);
-                        console.log("----");
-                    }
-                });
-        }
-    });
+//                         console.log("unutra 2 "+ kliknuto);
+//                         kliknuto = false;
+//                         console.log("unutra 2 end "+ kliknuto);
+//                         console.log("----");
+//                     }
+//                 });
+//         }
+//     });
 
 
 
